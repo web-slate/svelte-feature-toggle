fa37bdc813f7e1cab4caa0b6dec945412427c99e
Venkatraman.R <ramsunvtech@gmail.com>
2020-09-07 10:07:51 +0800

    tech(base): Add Components

---
+++ src/components/CovidInfo.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Field from "./Field.svelte";
+</script>
+
+<Field id="vaccineDate" label="Vaccination Date" />
+<Field id="vaccineLocation" label="Vaccination Location" />
+<Field id="waitingList" label="Are you waiting for Vaccine?" type="checkbox" />
---
+++ src/components/Field.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let type = "text";
+  export let id;
+  export let label;
+</script>
+
+<style>
+  label {
+    width: 160px;
+    display: inline-block;
+  }
+</style>
+
+<div><label for={id}>{#if label}{label}: {/if}</label> <input {type} {id} /> <br /> <br /></div>
